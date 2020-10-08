0a4a615218a80dd68a8b623d9c1d03dfb62eacac
Bryan Anthua <bryananthua.m@gmail.com>
2020-10-08 10:05:57 -0500

    Added basic middlewares

--- server.js
+++ server.js
@@ -5,9 +5,31 @@ const express = require('express');
 const app = express();
 
 // --- CONFIGURATIONS
+app.use(express.json()); // only looks at requests where the Content-Type: application/json
+
+// Middleware: CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+// Middleware: Root Point
 app.use('/', (req, res, next) => {
     res.write('BBVA API');
     res.end();
+});
+
+// Middleware: Handling Errors
+app.use((error, req, res, next) => {
+    const statusCode = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(statusCode).json({
+        message: message,
+        data: data
+    });
 });
 
 // --- SERVER
